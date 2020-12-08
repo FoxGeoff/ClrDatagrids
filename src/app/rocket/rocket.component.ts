0daa3c0e56ca81a5b8bb2c757273b6374185c6f3
Geoff Fox <fox.geoffrey@gmail.com>
2020-12-07 17:50:05 -0800

    Add Rocket component code

--- src/app/rocket/rocket.component.ts
+++ src/app/rocket/rocket.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-rocket',
   templateUrl: './rocket.component.html',
-  styleUrls: ['./rocket.component.sass']
+  styleUrls: ['./rocket.component.sass'],
+  animations: [
+    trigger('flyIn', [
+      state('in', style({ top: '100%', opacity: 1 })),
+      transition('void => *', [
+        group([
+          animate('5s ease', style({ top: '-30%' })),
+          animate('5s 1s ease', style({ opacity: 0 })),
+        ])
+      ])
+    ])
+  ]
 })
 export class RocketComponent implements OnInit {
+  state: any;
+  @Input() open = false;
+  @Input() name = '';
 
   constructor() { }
 
